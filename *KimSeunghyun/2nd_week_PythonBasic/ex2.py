dca1694a1dc4055eda64cf76d1cb8a653348fd7c
kelon <jojo600@gmail.com>
2016-01-30 18:00:39 +0900

    commit

--- *KimSeunghyun/2nd_week_PythonBasic/ex2.py
+++ *KimSeunghyun/2nd_week_PythonBasic/ex2.py
@@ -1,25 +1,25 @@
-class Cal(object):
-    def __init__(self, v1, v2):
-        if isinstance(v1, int):
-            self.v1 = v1
-        if isinstance(v2, int):
-            self.v2 = v2
-    def add(self):
-        return self.v1+self.v2
-    def subtract(self):
-        return self.v1-self.v2
-    def setV1(self, v):
-        if isinstance(v, int):
-            self.v1 = v
-        else:
-            print("error")
-    def getV1(self):
-        return self.v1
+class Class1(object):
+    def method1(self):
+        return 'm1'
 
-c1 = Cal(10, 10)
-print(c1.add())
-print(c1.subtract())
-c1.setV1('one')
-c1.v2 = 30
-print(c1.add())
-print(c1.subtract())
+c1 = Class1()
+print(c1, c1.method1())
+
+
+class Class3(Class1):
+    def method2(self):
+        return 'm2'
+
+c3 = Class3()
+print(c3, c3.method1())
+print(c3, c3.method2())
+
+class Class2(object):
+    def method1(self):
+        return 'm1'
+    def method2(self):
+        return 'm2'
+
+c2 = Class2()
+print(c2.method1())
+print(c2.method2())
